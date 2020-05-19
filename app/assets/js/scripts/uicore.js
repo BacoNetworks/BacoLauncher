1ddfe7d7d7f3bd8823db89447f2cfbab093dcf9e
Ignant <54343011+Ignant@users.noreply.github.com>
2020-05-19 15:48:13 -0700

    added image and some updates

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -48,7 +48,7 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/BacoNetworks/BacoLauncher/releases/download/v${info.version}/BacoLauncher-${info.version}.dmg`
+                    info.darwindownload = `https://github.com/BacoNetworks/BacoLauncher/releases/download/v${info.version}/BacoLauncher-setup-${info.version}.dmg`
                     showUpdateUI(info)
                 }
                 
